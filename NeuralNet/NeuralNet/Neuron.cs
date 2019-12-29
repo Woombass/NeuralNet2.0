7a2f9619c016fd083bb1c1c195012b66a27ba3bc
I Am THe SuperStar <pw333t@gmail.com>
2019-12-29 23:27:29 +0400

    Начальная реализация простейшего нейрона.

---
+++ NeuralNet/NeuralNet/Neuron.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace NeuralNet
+{
+    public class Neuron
+    {
+        public List<double> Weight { get;}
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Hidden)
+        {
+            NeuronType = type;
+            Weight = new List<double>();
+            for (int i = 0; i < inputCount; i++)
+            {
+                Weight.Add(1.0);
+            }
+        }
+
+        public double FeedForward(List<double> signalsList)
+        {
+            #region Проверки
+
+            if (signalsList.Count != Weight.Count)
+            {
+                throw new ArgumentOutOfRangeException("Количество сигналов не совпадает с количеством входов!");
+            }
+
+            #endregion
+
+
+            double sum = 0.0;
+            for (int i = 0; i < signalsList.Count; i++)
+            {
+                sum += signalsList[i] * Weight[i];
+            }
+
+            Output = Sigmoid(sum);
+
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            var result = 1.0 / (1 + Math.Exp(-x));
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
